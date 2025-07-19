35290330648321be653ccec1f208645985e7f071
Avinash Kumar <119006127+avinashkumar71@users.noreply.github.com>
2025-07-19 13:36:21 +0530

    Update game.js

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ const stopWatchTimer = document.getElementById("stopwatch-timer");
 
 let score = 0 ;
 let bugInterval;
+let WrongBugInterval ; 
 let Time = 31;
 let TimeInterval;
 
@@ -16,6 +17,7 @@ startButton.addEventListener('click',()=>{
         TimeInterval = setInterval(()=>{
             if(Time===0){
                 clearInterval(bugInterval)
+                clearInterval(WrongBugInterval)
                 clearInterval(TimeInterval)
                 stopWatchTimer.textContent = "X"
                 startButton.textContent = 'start';
@@ -32,9 +34,11 @@ startButton.addEventListener('click',()=>{
         startButton.textContent = 'stop';
         startButton.style.backgroundColor = 'red'
         bugInterval = setInterval(createBug,1000)
+        WrongBugInterval = setInterval(createWrongBug,2000)
 
     }else if(startButton.innerText==='stop'){
         clearInterval(bugInterval)
+        clearInterval(WrongBugInterval)
         stopWatchTimer.textContent = "X"
         Time = 31
         score = 0
@@ -69,6 +73,32 @@ function createBug() {
         scoreEl.textContent = `${score}`;
   });
 
+    gameArea.appendChild(bug);
+}
+
+function createWrongBug(){
+    const bug = document.createElement("span");
+    bug.textContent = "🪲";
+    bug.classList.add("bug");
+    bug.style.color = 'red'
+
+    const a = Math.random() * (gameArea.clientWidth - 40);
+    const b = Math.random() * (gameArea.clientHeight - 40);
+
+    bug.style.left = `${a}px`;
+    bug.style.top = `${b}px`;
+
+    const timeoutId = setTimeout(() => {
+        bug.remove();
+    }, 700);
+
+    bug.addEventListener("click", () => {
+        bug.remove();
+        document.getElementById("pop-sound").play();
+        clearTimeout(timeoutId);
+        score--;
+        scoreEl.textContent = `${score}`;
+  });
     gameArea.appendChild(bug);
 }
 
